0f19b9535ece8d5e50874be47cc243ba70429b47
1dxrpz <Scaphoidederps@gmail.ru>
2021-04-29 23:45:31 +0300

    fix

--- UniversityProject/Game1.cs
+++ UniversityProject/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading.Tasks;
+using UniversityProject.server;
 
 namespace UniversityProject
 {
@@ -30,11 +32,20 @@ namespace UniversityProject
 			// TODO: use this.Content to load your game content here
 		}
 
+		async void Connect()
+		{
+			await Task.Run(() => Client.Connect());
+		}
+		bool start = true;
 		protected override void Update(GameTime gameTime)
 		{
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
-
+			if (start)
+			{
+				Connect();
+				start = false;
+			}
 
 			base.Update(gameTime);
 		}
