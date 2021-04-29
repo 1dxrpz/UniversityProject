f69925e04d26c8492f52b8ee6efe4ab736815022
1dxrpz <Scaphoidederps@gmail.ru>
2021-04-29 23:32:16 +0300

    feat(client): added tcp client

--- UniversityProject/Game1.cs
+++ UniversityProject/Game1.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading.Tasks;
+using UniversityProject.server;
 
 namespace UniversityProject
 {
 	public class Game1 : Game
 	{
-		private GraphicsDeviceManager _graphics;
+		private GraphicsDeviceManager graphics;
 		private SpriteBatch _spriteBatch;
 
 		public Game1()
 		{
-			_graphics = new GraphicsDeviceManager(this);
+			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
 			IsMouseVisible = true;
 		}
@@ -19,30 +21,39 @@ namespace UniversityProject
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
-
 			base.Initialize();
 		}
 
+		SpriteFont font;
 		protected override void LoadContent()
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
-
-			// TODO: use this.Content to load your game content here
+			font = Content.Load<SpriteFont>("font");
+			
 		}
-
+		bool start = true;
+		async void Connect()
+		{
+			await Task.Run(() => Client.Connect());
+		}
 		protected override void Update(GameTime gameTime)
 		{
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
-
-
+			if (start)
+			{
+				Connect();
+				start = false;
+			}
 			base.Update(gameTime);
 		}
 
 		protected override void Draw(GameTime gameTime)
 		{
 			GraphicsDevice.Clear(Color.Tomato);
-
+			_spriteBatch.Begin();
+			//_spriteBatch.DrawString(font, Client.GetValue(), new Vector2(10, 10), Color.Black);
+			_spriteBatch.End();
 			base.Draw(gameTime);
 		}
 	}
